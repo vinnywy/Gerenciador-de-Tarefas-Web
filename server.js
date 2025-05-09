29ea5633694225836c02fbd620a041463fa775d4
Vinícius Rangel <vinicius.marques@sou.inteli.edu.br>
2025-05-09 09:35:38 -0300

    feat new files

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 require('dotenv').config();
 const express = require('express');
 const app = express();
@@ -38,3 +39,20 @@ db.connect()
   .catch(err => {
     console.error('Erro ao conectar ao banco de dados:', err);
   });
+=======
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Middleware para processar JSON
+app.use(express.json());
+
+// Rotas
+const routes = require('./routes/index');
+app.use('/', routes);
+
+// Inicializa o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+>>>>>>> Stashed changes

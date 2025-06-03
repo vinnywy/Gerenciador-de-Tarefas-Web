e4a4a65e923887a5541ca9bec48bc53e9e8db023
Vinícius Rangel <vinicius.marques@sou.inteli.edu.br>
2025-06-03 09:36:46 -0300

    feat: Atualiza .env

--- routes/index.js
+++ routes/index.js
@@ -3,3 +3,13 @@ const router = express.Router();
 const homeController = require("../controllers/homeController");
 router.get ("/", homeController.index);
 module.exports = router ;
+
+const TarefaController = require('../controllers/TarefaController');
+
+// Rotas para o CRUD de tarefas
+router.post('/tarefas', TarefaController.criarTarefa);
+router.get('/tarefas', TarefaController.listarTarefas);
+router.put('/tarefas/:id', TarefaController.editarTarefa);
+router.delete('/tarefas/:id', TarefaController.excluirTarefa);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -13,3 +13,22 @@ app.use('/', routes);
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const routes = require('./routes');
+
+const port = 3000;
+
+// Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+
+// Usando as rotas definidas
+app.use('/api', routes);
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
